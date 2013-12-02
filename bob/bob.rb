68df296b17119da982ac40a80005601814f174f1
Ashton McCrate <a.mccrate@gmail.com>
2013-12-01 21:49:13 -0500

    refactor bob

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,12 +1,18 @@
 class Bob
   def hey(statement)
-    
-    case 
-    when statement.strip.empty? then "Fine. Be that way!"
-    when (statement.match(/.*[A-Z]+.*/) && !statement.match(/.*[a-z].*/)) then "Woah, chill out!"
-    when statement.end_with?("?") then "Sure."
+    case type_of_statment(statement)
+    when "empty" then "Fine. Be that way!"
+    when "shouting" then "Woah, chill out!"
+    when "question" then "Sure."
     else "Whatever."
     end
-
+  end
+  
+  def type_of_statment(statement)
+    case statement
+    when statement.strip.empty? then "empty"
+    when (statement.match(/.*[A-Z]+.*/) && !statement.match(/.*[a-z].*/)) then "shouting"
+    when statement.end_with?("?") then "question"
+    end
   end
 end
