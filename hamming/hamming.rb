8d9e6b1fb6243ddf4ece9ffbc7ebb3652ec9b48d
Ashton McCrate <a.mccrate@gmail.com>
2013-12-01 21:50:20 -0500

    refactor hamming'

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,14 +2,11 @@ class Hamming
   def self.compute(a, b) 
     dna_strand1 = a.chars
     dna_strand2 = b.chars
-    normalize(dna_strand1, dna_strand2)
-    #return a count of how many elemets are different
-    (0...@amount_to_compare).count do |nucleotide| 
-      dna_strand1[nucleotide] != dna_strand2[nucleotide]
+    find_common_length(dna_strand1, dna_strand2).times.count do |nucleotide_position| 
+      dna_strand1[nucleotide_position] != dna_strand2[nucleotide_position]
     end
   end
-  #determine how many elements to iterate over
-  def self.normalize(array1, array2)
-      @amount_to_compare = [array1.length, array2.length].min  
+  def self.find_common_length(array1, array2)
+     [array1.length, array2.length].min  
   end
 end
